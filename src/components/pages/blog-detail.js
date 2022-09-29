048737e4959c8b6248ef540de13e771576ffc563
Jeremy Crundwell <jeremyfish32@gmail.com>
2022-09-29 12:42:43 -0700

    added blog edit title

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -3,14 +3,23 @@ import axios from "axios";
 import ReactHtmlParcer from "react-html-parser";
 import BlogFeaturedImage from '../blog/blog-featured-image';
 
+import BlogForm from "../blog/blog-form"
 export default class BlogDetail extends Component {
     constructor(props) {
         super(props);
 
         this.state= {
             currentId: this.props.match.params.slug,
-            blogItem: {}
-        }
+            blogItem: {},
+            editMode: false,
+        };
+
+        this.handleEditClick = this.handleEditClick.bind(this)
+    }
+
+
+    handleEditClick() {
+        this.setState({ editMode: true}) ;
     }
 
 
@@ -38,19 +47,26 @@ export default class BlogDetail extends Component {
             blog_status
         } = this.state.blogItem
 
-
-        return (
-            <div className="blog-container">
+        const contentManager = () => {
+            if (this.state.editMode) {
+                return <BlogForm editMode={this.state.editMode} blog={this.state.blogItem} />
+            } else {
+                return(
                 <div className="content-container">
-                    <h1>{title}</h1>
+                    <h1 onClick={this.handleEditClick}>{title}</h1>
 
                     <BlogFeaturedImage img={featured_image_url} />
 
                     <div className ="content">
                         {ReactHtmlParcer(content)}
                     </div>
-                </div>
-            </div>
+                </div>)
+            }
+        }
+
+
+        return (
+            <div className="blog-container">{contentManager()}</div>
         )
     }
 }
